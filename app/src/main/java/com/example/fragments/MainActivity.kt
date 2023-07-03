f1c8ec38a7efe008d09ef20a923cae3d8ff08155
ihusamali <husamali6300@gmail.com>
2023-07-03 15:39:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, firstFragment)
+            commit()
+        }
+
+        val btnFragment1 = findViewById<Button>(R.id.btn_fragment1)
+        val btnFragment2 = findViewById<Button>(R.id.btn_fragment2)
+
+        btnFragment1.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnFragment2.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+    }
+}
